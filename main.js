c87c9f1b9fa4c4486f0bcbe8a5559ff2ee6ea0b6
Polina <polinaalejnikowa@gmail.com>
2020-12-16 16:02:42 +0300

    add content_1

--- main.js
+++ main.js
@@ -1,72 +1,35 @@
-//screen 5 
-let bookClose = document.getElementsByClassName('book_close_bg')[0],
-    tableClose = document.getElementsByClassName('table_close')[0],
-    blockBooks = document.getElementsByClassName('books')[0],
-    page = document.getElementById('page'),
-    pageRight = document.getElementById('pageRight'),
-    backPortal = document.getElementsByClassName('back-portal')[0],
-    tabeleOpen = document.getElementsByClassName('tabele_open')[0],
-    blockQuestion_1 = document.querySelector('.question_1'),
-    blockQuestion_2 = document.querySelector('.question_2');
-
-bookClose.addEventListener('click', ()=> {
-    
-    setTimeout(()=> bookClose.classList.add('book-play'), 100);
-    setTimeout(()=> bookClose.classList.add('book-opasity'), 200);
-
-    setTimeout(()=> page.classList.remove('none'), 700);
-    setTimeout(()=> pageRight.classList.remove('none'), 200);
-
-    setTimeout(()=> page.style.left = '554px', 100);
-
-
-    setTimeout(()=> page.classList.add('page-play'), 100);
-    // setTimeout(()=> page.classList.add('transparent'), 400);
-
-    // setTimeout(()=> backPortal.classList.add('opacity'), 1000);
-
-
-
-    setTimeout(()=> tableClose.classList.add('none'), 500);
-
-    setTimeout(()=> tabeleOpen.classList.remove('none'), 500);
-    setTimeout(()=> tabeleOpen.classList.add('flex'), 500);
-
-    setTimeout(()=> blockQuestion_1.classList.add('opacity'), 700);
-
-
-    setTimeout(()=> page.classList.remove('page-play'), 1300);
-
-
-    // tableClose.classList.add('none');
-    // blockBooks.classList.add('table-screen5');
-    // tabeleOpen.classList.remove('none');
-    // tabeleOpen.classList.add('flex');
-    timer();
-});
-
-//item-image page 1
-
-let answerBlock = document.querySelector(".answer"),
+let blockQuestion_1 = document.querySelector('.question_1'),
+    blockQuestion_2 = document.querySelector('.question_2'),
+    blockQuestion_3 = document.querySelector('.question_3'),
+    blockQuestion_4 = document.querySelector('.question_4'),
+    blockQuestion_5 = document.querySelector('.question_5'),
+    blockQuestion_6 = document.querySelector('.question_6'),
+    blockQuestion_7 = document.querySelector('.question_7'),
+    blockQuestion_8 = document.querySelector('.question_8'),
+    blockEndQuestions = document.querySelector('.blockEndQuestions'),
+    answerBlock_1 = document.querySelector(".answer"),
+    answerBlock_2 = document.querySelector(".answer_2"),
+    answerBlock_3 = document.querySelector(".answer_3"),
+    answerBlock_4 = document.querySelector(".answer_4"),
+    answerBlock_5 = document.querySelector(".answer_5"),
+    answerBlock_6 = document.querySelector(".answer_6"),
+    answerBlock_7 = document.querySelector(".answer_7"),
+    answerBlock_8 = document.querySelector(".answer_8"),
     blockZoom = document.querySelector('.arm-block'),
     blockArm = document.querySelector('.arm'),
     close = document.querySelector('.close'),
-    partRight = document.querySelector('.book_open'),
-    helpMark = document.querySelector('.help-mark'),
-    help = document.querySelector('.help');
-    t = 0;
-
-    // partRight.addEventListener('click', (event)=> {
-    //     let target = event.target;
-    //     console.log(target)
-    //     if (target.classList.contains('help-mark')) {
-    //         help.classList.add('help-active');
-    //         helpMark.classList.add('help-mark-active');
-    //     }
-// })    
+    bookOpen = document.querySelector('.book_open'),
+    tabeleOpen = document.querySelector('.tabele_open'),
+    textTimer = document.getElementById('timer'),
+    banana = document.querySelector('#banana'),
+    timePlus = document.querySelector('#plusTime'),
+    seconds = 0,
+    timerId,
+    endTime = document.getElementById('end-time');
     
+timer();  
 
-answerBlock.addEventListener('click', (event)=> {
+answerBlock_1.addEventListener('click', (event)=> {
     let target = event.target;
        
     if (target.classList.contains('answer-choose')) {
@@ -85,7 +48,6 @@ answerBlock.addEventListener('click', (event)=> {
             hendImg = document.querySelector('.hand-img');
 
         hendImg.style.background = `url(./images/deck/arm/backg/${keyBackg}.png) no-repeat`;
-        console.log();
 
         let timerIdBottom = setInterval(() => blockZoom.style.bottom = '0', 10);
         let timerId = setInterval(() => blockZoom.style.transform = 'rotate(0)', 20);
@@ -95,8 +57,35 @@ answerBlock.addEventListener('click', (event)=> {
     }
 });
 
+answerBlock_2.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_3.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_4.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_5.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_6.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_7.addEventListener('click', (event)=> addActiveClass(event));
+answerBlock_8.addEventListener('click', (event)=> addActiveClass(event));
 
 
+function addActiveClass(event) {
+    let target = event.target;
+
+    if ( target.classList.contains('answer-item') ) {
+        let children = target.childNodes,
+            number = children[1];
+
+        number.classList.toggle('answer_number_active');
+        target.classList.toggle('active');
+    }
+    if ( target.classList.contains('answer-text') ) {
+        let parent = target.parentNode,
+            children = parent.childNodes,
+            number = children[1];
+
+        number.classList.toggle('answer_number_active');
+        parent.classList.toggle('active');
+    }
+}    
+ 
 close.addEventListener('click', ()=> {
     let timerIdBottom = setInterval(() => blockZoom.style.bottom = '-650px', 10);
     let timerId = setInterval(() => blockZoom.style.transform = 'rotate(-90deg)', 10);
@@ -106,16 +95,181 @@ close.addEventListener('click', ()=> {
 });
 
 
-//btn next page 1
-let btnAnswer = document.querySelector('.answer-btn-question_1'),
-    prarent = btnAnswer.parentNode,
-    spanBtnAnswer = document.getElementById('answer'),
-    spanBtnNext = document.getElementById('next');
+bookOpen.addEventListener('click', (event)=> {
+    let target = event.target;
+    if (target.classList.contains('help-mark')) {
+
+        let parent = target.parentNode,
+            videoBlock = parent.nextElementSibling,
+            parentParent = parent.parentNode,
+            partRight = parentParent.childNodes[3],
+            videoBg = videoBlock.nextElementSibling,
+            helpClose = target.nextElementSibling,
+            key = target.dataset.key,
+            video = document.getElementById(key);
+         
+        parent.classList.add('help-active');
+        setTimeout(()=> videoBlock.classList.add('opacity'), 300);
+        setTimeout(()=> partRight.classList.add('help_video'),300);
+        setTimeout(()=> helpClose.classList.add('opacity'), 200);
+        setTimeout(()=> videoBg.classList.add('opacity'), 200);
+        setTimeout(()=> video.play(), 400);
+    }
+
+    if (target.classList.contains('help-close')) {
+
+        let parent = target.parentNode,
+            videoBlock = parent.nextElementSibling,
+            parentParent = parent.parentNode,
+            partRight = parentParent.childNodes[3],
+            key = target.dataset.key,
+            videoBg = videoBlock.nextElementSibling,
+            video = document.getElementById(key);
+
+        parent.classList.remove('help-active');
+        parent.classList.add('bg-help');
+        video.pause();
+        video.currentTime = '0';
+        target.classList.remove('opacity');
+        setTimeout(()=> videoBg.classList.remove('opacity'), 100);
+        setTimeout(()=> videoBlock.classList.remove('opacity'), 100);
+        setTimeout(()=> partRight.classList.remove('help_video'),200);
+    }
+})
+
+let btnAnswer_1 = document.querySelector('.answer-btn-question_1'),
+    btnAnswer_2 = document.querySelector('.answer-btn-question_2'),
+    btnAnswer_3 = document.querySelector('.answer-btn-question_3'),
+    btnAnswer_4 = document.querySelector('.answer-btn-question_4'),
+    btnAnswer_5 = document.querySelector('.answer-btn-question_5'),
+    btnAnswer_6 = document.querySelector('.answer-btn-question_6'),
+    btnAnswer_7 = document.querySelector('.answer-btn-question_7'),
+    btnAnswer_8 = document.querySelector('.answer-btn-question_8');
 
 
+btnAnswer_1.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer'),
+        dataAtt = target.dataset.btn;
 
-spanBtnAnswer.addEventListener ('click', ()=> {
-        let blocksActive = prarent.getElementsByClassName('answer-item'),
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_1,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_1,blockQuestion_2);
+    }
+})
+
+btnAnswer_2.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_2'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_2,spanBtnAnswer);
+        console.log(btnAnswer_2)
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_2,blockQuestion_3);
+    }
+})
+
+btnAnswer_3.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_3'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_3,spanBtnAnswer);
+        console.log(btnAnswer_3)
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_3,blockQuestion_4);
+    }
+})
+
+btnAnswer_4.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_4'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_4,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_4,blockQuestion_5);
+    }
+})
+
+btnAnswer_5.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_5'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_5,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_5,blockQuestion_6);
+    }
+})
+
+btnAnswer_6.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_6'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_6,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_6,blockQuestion_7);
+    }
+})
+
+btnAnswer_7.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_7'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_7,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(blockQuestion_7,blockQuestion_8);
+    }
+})
+
+btnAnswer_8.addEventListener('click', (event)=> {
+    let target = event.target,
+        parentTarget = target.parentNode,
+        spanBtnAnswer = parentTarget.querySelector('#answer_8'),
+        dataAtt = target.dataset.btn;
+
+    if (dataAtt == 'answer') {
+        checkItemAnswer(btnAnswer_8,spanBtnAnswer);
+    }
+    if (dataAtt == 'next') {
+        showNextPage(bookOpen,blockEndQuestions);
+        blockZoom.classList.add('none');
+        blockEndQuestions.classList.remove('none');
+        tabeleOpen.classList.add('none');
+        endTime.innerHTML = creatTime();
+    }
+})
+
+
+function checkItemAnswer(btn,spanBtnAnswer) {
+    let parent = btn.parentNode,
+        blockItems = parent.querySelector('.answer-items'), 
+        blocksActive = blockItems.getElementsByClassName('answer-item'),
         arrAnswer = [];
 
     for (let i = 0; i < blocksActive.length; i++) {
@@ -139,74 +293,55 @@ spanBtnAnswer.addEventListener ('click', ()=> {
     }
 
     let result = arrAnswer.every(isCorrect);
-    checkAnswer();
 
-    function checkAnswer() {
-        let banana = document.querySelector('.banana');
-        if (result == true) {
-            banana.style.background = 'url(./images/deck/screen_5/question_monkey_yes.png) no-repeat';
-            spanBtnAnswer.classList.add('none');
-            spanBtnNext.classList.remove('none');
-            plusTime(t);
-        } else {
-            t = 2;
-            plusTime(t);
-            banana.style.background = 'url(./images/deck/screen_5/question_monkey_no.png) no-repeat';
-            setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/banana-wait-2-x@2x.png) no-repeat', 1000);
-            setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/question_monkey_again.png) no-repeat', 2000);
-            setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/question_monkey_d.png) no-repeat', 3000);
-        }
+    if (result == true) {
+        banana.style.background = 'url(./images/deck/screen_5/question_monkey_yes.png) no-repeat';
+        setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/question_monkey_d.png) no-repeat', 1000);
+        
+        let spanBtnNext = parent.querySelector('.goToNextQuestion');
+        spanBtnAnswer.classList.add('none');
+        spanBtnNext.classList.remove('none');
+    } else {
+        timePlus.classList.add('time_plus');
+        timePlus.classList.add('opacity');
+        setTimeout(()=> timePlus.classList.remove('opacity'), 1000);
+        setTimeout(()=> timePlus.classList.remove('time_plus'), 2000);
+        banana.style.background = 'url(./images/deck/screen_5/question_monkey_no.png) no-repeat';
+        setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/banana-wait-2-x@2x.png) no-repeat', 1000);
+        setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/question_monkey_again.png) no-repeat', 2000);
+        setTimeout(()=> banana.style.background = 'url(./images/deck/screen_5/question_monkey_d.png) no-repeat', 3000);
+        plusTime()
     }
-})
-
-spanBtnNext.addEventListener ('click', ()=> {
-
-
-    blockQuestion_1.classList.add('none');
-    page.style.zIndex = '10';
-    page.classList.remove('page-play');
-    showNextPage();
-
-});
-
-
-function showNextPage() {
-    setTimeout(()=> page.classList.add('page-play'), 100);
-    setTimeout(()=> page.style.left = '578px', 100);
-
-    setTimeout(()=> page.style.zIndex = '0', 100);
-
-    // setTimeout(()=> page.classList.add('transparent'), 1000);
-    setTimeout(()=> blockQuestion_2.classList.add('opacity'), 900);
-    setTimeout(()=> blockQuestion_2.classList.remove('none'), 900);
 }
 
-//item-image page 2
-
+function showNextPage(prevBlock, nextBlock) {
+    setTimeout(()=> prevBlock.classList.add('none'), 100);
+    setTimeout(()=> nextBlock.classList.remove('none'), 200);
+}
 
 //timer
+let min = document.getElementById('min'),
+minutes = +min.dataset.min;
 
-let text = document.getElementById('timer'),
-    sec = document.getElementById('sec'),
-    min = document.getElementById('min'),
-    seconds = +sec.dataset.sec,
-    minutes = +min.dataset.min,
-    // minutes = plusTime(),
-    // plus = +text.dataset.plus,
-    timerId; 
-
-    console.log(minutes);
+function plusTime() {
+    minutes = +min.dataset.min;
+    min.innerHTML = minutes + 2;
+    min.dataset.min = minutes + 2;
+    minutes = minutes + 2;
+}
 
 function addValue() {
+
     seconds++;
         if (seconds >= 60) {
             seconds = 0;
+            minutes++;
         }
         if (minutes == 60) {
             clearTimeout(timerId);
             return;
         }
-    text.innerHTML = creatTime();
+    textTimer.innerHTML = creatTime();
     timer();
 }
 
@@ -219,12 +354,3 @@ function creatTime() {
     return html;
 }
 
-// plusTime(0);
-function plusTime(t) {
-    // let  min = document.getElementById('min'),
-    //     data = min.dataset.min;
-    //     min.innerHTML= '3'
-    // console.log(data)
-    return t;
-
-}
